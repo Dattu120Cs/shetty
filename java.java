0257aab860709f11d6bf45168b5c974598de8aab
Dattu120Cs <dattu129nandyal@gmail.com>
2024-05-16 09:20:19 +0530

    update

--- java.java
+++ java.java
@@ -2,5 +2,6 @@ public class java{
 	public static void main(String[] args) {
 		System.out.println("welcome to gpt college");
         System.out.println("welcome gpt college");
+		System.out.println("welcome dploma college");
 	}
 }
